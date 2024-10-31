bfbad27419bec1129e032d881b5a1b4e51afffbe
Jaydevsinh Himmatsinh Parmar <jparmar1742@conestogac.on.ca>
2024-10-31 16:36:46 -0400

    Commit 3: Player1 wins, if PaperVsRock

--- Assignment-3 TDD/Source.c
+++ Assignment-3 TDD/Source.c
@@ -6,7 +6,8 @@ const char* RockPaperScissors(const char* Player1, const char* Player2)
 {
 	// Determining the outcomes
 	if ((strcmp(Player1, "Rock") == 0 && strcmp(Player2, "Scissors") == 0) ||
-		(strcmp(Player1, "Scissors") == 0 && strcmp(Player2, "Paper") == 0)) 
+		(strcmp(Player1, "Scissors") == 0 && strcmp(Player2, "Paper") == 0) ||
+		(strcmp(Player1, "Paper") == 0 && strcmp(Player2, "Rock") == 0))
 	{
 		return "Player1";
 	}
