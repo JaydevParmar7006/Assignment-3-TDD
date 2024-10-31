9adf17759a4858f66a8d520f5c2fe89e564b8744
Jaydevsinh Himmatsinh Parmar <jparmar1742@conestogac.on.ca>
2024-10-31 10:45:26 -0400

    Initial Project Setup

---
+++ RockPaperScissorsTest/RockPaperScissorsTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RockPaperScissorsTest
+{
+	TEST_CLASS(RockPaperScissorsTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
