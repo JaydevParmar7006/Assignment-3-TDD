3e171730f72b2c6ac6a562a43e74ee2e54bd4067
Jaydevsinh Himmatsinh Parmar <jparmar1742@conestogac.on.ca>
2024-10-31 16:54:16 -0400

    Commit 7: commiting all Draw cases

--- Assignment-3 TDD/Source.c
+++ Assignment-3 TDD/Source.c
@@ -5,16 +5,21 @@
 const char* RockPaperScissors(const char* Player1, const char* Player2)
 {
 	// Determining the outcomes
-	if ((strcmp(Player1, "Rock") == 0 && strcmp(Player2, "Scissors") == 0) ||
-		(strcmp(Player1, "Scissors") == 0 && strcmp(Player2, "Paper") == 0) ||
-		(strcmp(Player1, "Paper") == 0 && strcmp(Player2, "Rock") == 0))
-	{
-		return "Player1";
-	}
-	else if ((strcmp(Player1, "Scissors") == 0 && strcmp(Player2, "Rock") == 0) ||
-		(strcmp(Player1, "Paper") == 0 && strcmp(Player2, "Scissors") == 0) ||
-		(strcmp(Player1, "Rock") == 0 && strcmp(Player2, "Paper") == 0))
-	{
-		return "Player2";
-	}
+    if (strcmp(Player1, Player2) == 0) 
+    {
+        return "Draw";
+    }
+    else if ((strcmp(Player1, "Rock") == 0 && strcmp(Player2, "Scissors") == 0) ||
+        (strcmp(Player1, "Scissors") == 0 && strcmp(Player2, "Paper") == 0) ||
+        (strcmp(Player1, "Paper") == 0 && strcmp(Player2, "Rock") == 0)) 
+    {
+        return "Player1";
+    }
+    else if ((strcmp(Player1, "Scissors") == 0 && strcmp(Player2, "Rock") == 0) ||
+        (strcmp(Player1, "Paper") == 0 && strcmp(Player2, "Scissors") == 0) ||
+        (strcmp(Player1, "Rock") == 0 && strcmp(Player2, "Paper") == 0)) 
+    {
+        return "Player2";
+    }
+	
 }
